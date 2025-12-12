b16f0491103859baa42f62491451cf501ce1dca8
Daniel Queirós <danielqueiros786@gmail.com>
2025-12-12 14:49:07 +0000

    Configuração inicial do projeto

---
+++ equipa14-componente3/src/main/java/lp/JavaFxClient/controllers/FromController.java
@@ -0,0 +1,23 @@
+package lp.JavaFxClient.controllers;
+
+import javafx.fxml.FXML;
+
+public class FromController {
+
+    @FXML public void onCriarUtilizador() {}
+    @FXML public void onListarUtilizadores() {}
+    @FXML public void onApagarUtilizador() {}
+    @FXML public void onCriarTipo() {}
+    @FXML public void onCriarEvento() {}
+    @FXML public void onEditarEvento() {}
+    @FXML public void onApagarEvento() {}
+    @FXML public void onListarEventos() {}
+    @FXML public void onEventosPoucaAderencia() {}
+    @FXML public void onAprovarEvento() {}
+    @FXML public void onRejeitarEvento() {}
+    @FXML public void onInscreverEstudante() {}
+    @FXML public void onCancelarInscricao() {}
+    @FXML public void onSair() {
+        System.exit(0);
+    }
+}
